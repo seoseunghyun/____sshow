244c7681962e25ee7854e86a2a9e78e7ced82898
seoseunghyun <me@seunghyun.net>
2014-08-04 14:31:25 +0900

    SSHOW initialize
    SSHOW initialize

--- config/init.php
+++ config/init.php
@@ -5,10 +5,23 @@
 	me@seunghyun.net
 */
 
-// 기본 설정
-define( '_SSHOW_' , true);
-define( '_SSHOW_DIR' , str_replace( 'config\\' , '' , str_replace( 'config/' , '' , str_replace( basename(__FILE__) , '' , __FILE__ ) ) ) );
-define( '_SSHOW_VERSION_' , '0.01b');
+
+/*
+	기본 설정
+	php버전 5.0 이상을 지원합니다.
+	_SSHOW_*_ public 전역 상수
+	__SSHOW_*__ private 전역 상수
+*/
+define( '_SSHOW_' , true );
+define( '_SSHOW_DIR_' , str_replace( 'config\\' , '' , str_replace( 'config/' , '' , str_replace( basename(__FILE__) , '' , __FILE__ ) ) ) );
+define( '_SSHOW_URL_' , '' );
+define( '_SSHOW_VERSION_' , '0.01b' );
+
+define( '_SSHOW_JQUERY_VERSION_' , '1.11.1' );
+
+// jquery CDN 이용 시 설정을 바꿀 수 있다.
+define( '_SSHOW_JQUERY_DIR_' , _SSHOW_DIR_.'common/src/jquery.js' );
+define( '_SSHOW_JQUERY_URL_' , _SSHOW_URL_.'common/src/jquery.js' );
 
 // 설정 초기화
 $sshowConfig = new sshowClass_config();
@@ -22,10 +35,15 @@ class sshowClass_config
 	// 생성자
 	function __construct()
 	{
-		if( file_exists('') )
+		// 설정 파일이 있을 경우 설정 파일의 내용으로 초기화 한다.
+		if( file_exists( _SSHOW_DIR_.'data/common/init.xml' ) )
 		{
-			
+			// 설정 파일의 경로 : _SSHOW_DIR_.'data/common/init.xml'
+			$xml = simplexml_load_file( _SSHOW_DIR_.'data/common/init.xml' );
+			$this->lang = $xml->lang;
+			$this->html = $xml->html;
 		}
+		
 		$this->lang = 'ko';
 		$this->html = '4.1';
 
---
+++ include/setup/index.php
@@ -0,0 +1,39 @@
+<?php
+/*
+	SSHOW ( http://www.sshow.org )
+	Copyright By SeoSeungHyun ( http://www.seoseunghyun.com || http://www.seunghyun.net )
+	me@seunghyun.net
+*/
+
+// 설치 메인 화면
+define('__SSHOW_SETUP_DIR__', _SSHOW_DIR_.'include/setup/' );
+define('__SSHOW_SETUP_URL__', _SSHOW_URL_.'include/setup/' );
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="Generator" content="SSHOW" />
+<title>SSHOW Setup</title>
+
+<link rel="stylesheet" type="text/css" href="<?=__SSHOW_SETUP_URL__?>css/index.css" />
+
+<script type="text/javascript" src="<?=_SSHOW_JQUERY_URL_?>"></script>
+<script type="text/javascript" src="<?=__SSHOW_SETUP_URL__?>js/index.js"></script>
+
+</head>
+<body>
+
+<div id="grid">
+	<div id="logo_grid">
+		
+	</div>
+	<div id="content_grid">
+	
+	</div>
+</div>
+
+</body>
+</html>
